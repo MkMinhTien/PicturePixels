d00d49225efc683137b31b267dc202089a630342
MkMinhTien <60256636+MkMinhTien@users.noreply.github.com>
2020-01-24 22:11:59 +0700

    My Nodejs app

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express'),
-    app = express(),
-    https = require('https').Server(app).listen(92634);
+    app = express();
+    // https = require('https').Server(app).listen(92634);
 
-app.use('/js', express.static(__dirname + '/script'));
-app.use('/css', express.static(__dirname + '/style'));
+app.use('/js', express.static(__dirname + '/public/script'));
+app.use('/css', express.static(__dirname + '/public/style'));
 // app.use('/img', express.static('../image'));
 
-console.log('NodeJS NOW is RUNNING');
+//console.log('NodeJS NOW is RUNNING');
 
 app.get('/', function(req, res) {
 
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 
+})
+
+app.listen(60000, function() {
+    console.log('NodeJS NOW is RUNNING!')
 })

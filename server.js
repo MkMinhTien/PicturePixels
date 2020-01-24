33e703850b02ebe60c4ffe880271b85206bfc299
MkMinhTien <60256636+MkMinhTien@users.noreply.github.com>
2020-01-24 22:03:22 +0700

    My Nodejs app

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
     app = express(),
     https = require('https').Server(app).listen(92634);
 
-app.use('/js', express.static('../script'))
-app.use('/css', express.static('../style'))
-app.use('/img', express.static('../image'))
+app.use('/js', express.static(__dirname + '/script'));
+app.use('/css', express.static(__dirname + '/style'));
+// app.use('/img', express.static('../image'));
 
 console.log('NodeJS NOW is RUNNING');
 
